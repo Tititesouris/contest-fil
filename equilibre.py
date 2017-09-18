c61f3367a6f0d2ed87e7eedd7ff29c3f0c622d04
Tititesouris <tititesouris@laposte.net>
2017-09-18 09:39:31 +0200

    Equilibre works in O(n)

--- equilibre.py
+++ equilibre.py
@@ -1,27 +1,17 @@
-import timeit
-start_time = timeit.default_timer()
-
-def count_zone(a, b):
-    c = [0, 0]
-    for x in range(a, b):
-        c[list[x]] += 1
-    return c
-
-
-n = 9
-# n = int(input())
-# list = [int(x) for x in input().split()]
-list = [0, 1, 0, 0, 0, 1, 1, 1, 0]
+n = int(input())
+list = [int(x) for x in input().split()]
 
 max = 0
-for start in range(n):
-    for stop in range(n, -1, -1):
-        count = count_zone(start, stop)
-        if abs(count[0] - count[1]) == 0:
-            if count[0] + count[1] > max:
-                max = count[0] + count[1]
-
+maxes = {0: -1}
+balance = 0
+for i in range(n):
+    if list[i] == 0:
+        balance -= 1
+    else:
+        balance += 1
+    if balance in maxes:
+        if i - maxes[balance] > max:
+            max = i - maxes[balance]
+    else:
+        maxes[balance] = i
 print(max)
-
-elapsed = timeit.default_timer() - start_time
-print(elapsed)
