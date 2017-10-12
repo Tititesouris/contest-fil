e370d63e64eef9541bfe46fb425797a0a77241a4
Tititesouris <tititesouris@laposte.net>
2017-10-12 13:50:22 +0200

    MirroredRotation

--- chocolat.py
+++ chocolat.py
@@ -1,3 +1,10 @@
+import sys
+sys.setrecursionlimit(1500)
+
+a, b, c, d = map(int, input().split())
+values = {}
+
+
 def getParent(children):
     if children == []:
         return 0
@@ -8,29 +15,48 @@ def getParent(children):
         return (max(children) + 1) * -1
 
 
-def getVal(w, h, x, y, values):
+def getMirrorRotation(wIn, hIn, xIn, yIn):
+    if wIn < hIn:
+        w = hIn
+        h = wIn
+        x = yIn
+        y = wIn - xIn - 1
+    else:
+        w = wIn
+        h = hIn
+        x = xIn
+        y = yIn
+    if x > w // 2:
+        x = w - x - 1
+    if y > h // 2:
+        y = h - y - 1
+    return [w, h, x, y]
+
+
+def getVal(w, h, x, y):
+    w, h, x, y = getMirrorRotation(w, h, x, y)
     value = "[" + str(w) + " " + str(h) + " " + str(x) + " " + str(y) + "]"
+    assert x < w and y < h
     if value not in values:
-        # if w == h
         children = []
         for i in range(1, w):
-            if x - i >= 0:
-                children.append(getVal(w - i, h, x - i, y, values))
+            if i <= x:
+                children.append(getVal(w - i, h, x - i, y))
             else:
-                children.append(getVal(w - i, h, x, y, values))
+                children.append(getVal(i, h, x, y))
         for j in range(1, h):
-            if y - j >= 0:
-                children.append(getVal(w, h - j, x, y - j, values))
+            if j <= y:
+                children.append(getVal(w, h - j, x, y - j))
             else:
-                children.append(getVal(w, h - j, x, y, values))
+                children.append(getVal(w, j, x, y))
         values.update({value: getParent(children)})
     return values[value]
 
-
+print(getVal(a, b, c, d))
+'''
 print("test")
 n = 7
 for i in range(n):
     for j in range(n):
         print(getVal(n, n, i, j, {}))
-
-print(getVal(10, 7, 7, 3, {})) # devrait etre 11
+'''
