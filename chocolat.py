76ac1029a946965c366b2a0d168e2dddc8c587da
Tititesouris <tititesouris@laposte.net>
2017-10-14 16:57:16 +0200

    Started working on chocolate game

--- chocolat.py
+++ chocolat.py
@@ -2,18 +2,9 @@ import sys
 
 sys.setrecursionlimit(1500)
 
+print("Board size: ")
 a, b, c, d = map(int, input().split())
 values = {}
-
-
-def getParent(children):
-    if children == []:
-        return 0
-    negatives = [child for child in children if child <= 0]
-    if negatives != []:  # At least 1 negative (or == 0) child
-        return max(negatives) * -1 + 1
-    else:  # No negative child
-        return (max(children) + 1) * -1
 
 
 def getMirrorRotation(wIn, hIn, xIn, yIn):
@@ -40,26 +31,65 @@ def getVal(w, h, x, y):
     assert x < w and y < h
     if value not in values:
         if w == h and w % 2 == 1 and (
-                    (x == 0 and y == h // 2) or (y == 0 and x == w // 2) or (x == w // 2 and y == h - 1) or (
-                y == h // 2 and x == w - 1)):
-            values.update({value: w})
+                            (x == 0 and y == h // 2) or (y == 0 and x == w // 2) or (x == w // 2 and y == h - 1) or (
+                                y == h // 2 and x == w - 1)):
+            values.update({value: {"value": w, "move": []}})
         else:
-            children = []
+            maxPos = 0
+            maxNeg = -999999999
+            hasNeg = False
+            move = []
+
             for i in range(1, w):
                 if i <= x:
-                    children.append(getVal(w - i, h, x - i, y))
+                    val = getVal(w - i, h, x - i, y)
                 else:
-                    children.append(getVal(i, h, x, y))
+                    val = getVal(i, h, x, y)
+                if maxNeg < val <= 0:
+                    maxNeg = val
+                    hasNeg = True
+                    move = [i, 0]
+                elif 0 < maxPos < val:
+                    maxPos = val
+                    move = [i, 0]
             for j in range(1, h):
                 if j <= y:
-                    children.append(getVal(w, h - j, x, y - j))
+                    val = getVal(w, h - j, x, y - j)
                 else:
-                    children.append(getVal(w, j, x, y))
-            values.update({value: getParent(children)})
-    return values[value]
+                    val = getVal(w, j, x, y)
+                if maxNeg < val <= 0:
+                    maxNeg = val
+                    hasNeg = True
+                    move = [0, j]
+                elif 0 < maxPos < val:
+                    maxPos = val
+                    move = [0, j]
+
+            if move != []:
+                if hasNeg:
+                    val = maxNeg * -1 + 1
+                else:
+                    val = maxPos * -1 + 1
+            else:
+                val = 0
+            values.update({value: {"value": val, "move": move}})
+            return val
+
+    return values[value]["value"]
 
 
-print(getVal(a, b, c, d))
+print("Calculating best moves...")
+getVal(a, b, c, d)
+while True:
+    print("Board:", a, b, c, d)
+    print("It's my turn... :thinking:")
+    w, h, x, y = getMirrorRotation(a, b, c, d)
+    value = "[" + str(w) + " " + str(h) + " " + str(x) + " " + str(y) + "]"
+    v = values[value]
+    print(("I" if v > 0 else "You") + " can win in " + str(v) + " turns")
+
+    break
+
 '''
 n = 11
 for i in range(n):
